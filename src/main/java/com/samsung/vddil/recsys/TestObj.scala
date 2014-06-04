b0f15466e3e380ea89f6a6beef924b41a2c61749
Jiayu Zhou <jiayu.zhou@2030001968-jzho.sisa.samsung.com>
2014-06-03 18:52:59 -0700

    init submit

---
+++ src/main/java/com/samsung/vddil/recsys/TestObj.scala
@@ -0,0 +1,25 @@
+package com.samsung.vddil.recsys
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object TestObj {
+
+  def main(args: Array[String]): Unit = {
+    
+		  println("Hell, yeah...")
+    
+	    val sc = new SparkContext("local[4]", "TestWordCount")    
+	    
+	    val lines = sc.textFile("/Users/jiayu.zhou/workspace/data/wordcount/adv_alad.txt")
+	    
+	    val counts = lines.flatMap(_.split(" ")).map(e => (e, 1)).reduceByKey(_+_).map{case (e1, e2) => ""+ e1 + "\t" + e2 }
+//	    
+//	    counts.saveAsTextFile("/Users/jiayu.zhou/workspace/data/wordcount/adv_alad_cnt_eclipse_maven_proj.txt")
+//    
+    
+    
+  }
+
+}
