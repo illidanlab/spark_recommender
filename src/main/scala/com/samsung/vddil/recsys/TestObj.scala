f7e2d4fde8c1b82e35add9cbc3bae7f39768bc6c
Jiayu Zhou <jiayu.zhou@ksalum-540u.sisa.samsung.com>
2014-06-05 18:06:10 -0700

    daily update

---
+++ src/main/scala/com/samsung/vddil/recsys/TestObj.scala
@@ -0,0 +1,39 @@
+package com.samsung.vddil.recsys
+
+
+import com.samsung.vddil.recsys._
+
+import com.samsung.vddil.recsys.job._
+
+
+/**
+ * This is the main entrance of the program. Due to historical reasons. 
+ * 
+ * @author jiayu.zhou
+ */
+object TestObj {
+
+  def main(args: Array[String]): Unit = {
+    
+		val logger = Logger.logger
+    
+		var jobFileStr:String = "./jobs/test_job.xml" 
+		if (args.size > 0){
+		  jobFileStr = args(0)
+		  logger.info("Job file specified: " + jobFileStr)
+		}else{
+		  logger.warn("No job file specified. Used default job file: " + jobFileStr)
+		}
+		
+		val jobList : List[Job] = Job.readFromXMLFile(jobFileStr)
+		
+		for (job:Job <- jobList){
+		   logger.info("=== Running job: " + job + " ===")
+		   job.run()
+		   logger.info("=== Job Done: " + job + " ===")
+		}
+		
+		logger.info("All jobs are completed.")
+  }
+
+}
