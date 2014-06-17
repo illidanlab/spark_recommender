8cd967b99a3424fea10b6a513624ef12f38b2dd4
Mohit Sharma <sharm163@umn.edu>
2014-06-17 12:07:01 -0700

    fixed bug for item genre features

--- src/main/scala/com/samsung/vddil/recsys/feature/item/ItemFeatureGenre.scala
+++ src/main/scala/com/samsung/vddil/recsys/feature/item/ItemFeatureGenre.scala
@@ -13,24 +13,24 @@ import com.samsung.vddil.recsys.utils.HashString
 
 object ItemFeatureGenre  extends FeatureProcessingUnit{
   
-    val ITEM_SUB_GENRE_IND = 2
-    val ITEM_GENRE_IND = 3
-    val ITEM_ID_IND = 1
+    val ItemGenreInd = 2
+    val ItemIdInd = 1
     
-    val GENRE_ID_IND = 1
-    val GENRE_LANG_IND = 2
-    val GENRE_DESC_IND = 3
-    val GENRE_LANG_FILT = "en" //default language 
+    val GenreIdInd = 1
+    val GenreLangInd = 2
+    val GenreDescInd = 3
+    val GenreLangFilt = "en" //default language 
       
     val Param_GenreLang = "lang" 
     
     def processFeature(featureParams:HashMap[String, String], jobInfo:RecJob):FeatureResource = {
-        
+    		
+    	//get spark context
         val sc = jobInfo.sc
         
         // 1. Complete default parameters
         //  default parameter for genre: lang filtering. 
-        var param_GenreLang:String = GENRE_LANG_FILT
+        var param_GenreLang:String = GenreLangFilt
         if(featureParams.isDefinedAt(Param_GenreLang)){
             param_GenreLang = featureParams(Param_GenreLang)
         }
@@ -48,14 +48,14 @@ object ItemFeatureGenre  extends FeatureProcessingUnit{
         var fileName = jobInfo.resourceLoc(RecJob.ResourceLoc_RoviHQ) + jobInfo.trainDates(0) + "/genre*" 
         var genreMap = sc.textFile(fileName).map { line =>
           val fields = line.split('|')
-          (fields(GENRE_ID_IND), fields(GENRE_LANG_IND), fields(GENRE_DESC_IND))
+          (fields(GenreIdInd), fields(GenreLangInd), fields(GenreDescInd))
         }.filter(x => x._2 == param_GenreLang)        
         
         for (trainDate <- jobInfo.trainDates.tail) {
             fileName = jobInfo.resourceLoc(RecJob.ResourceLoc_RoviHQ) + trainDate + "/genre*"
             val nextGenreMap = sc.textFile(fileName).map { line =>
                 val fields = line.split('|')
-                (fields(GENRE_ID_IND), fields(GENRE_LANG_IND), fields(GENRE_DESC_IND))
+                (fields(GenreIdInd), fields(GenreLangInd), fields(GenreDescInd))
             }.filter(x => x._2 == param_GenreLang)
             genreMap = genreMap.union(nextGenreMap).distinct
         }  
@@ -72,48 +72,59 @@ object ItemFeatureGenre  extends FeatureProcessingUnit{
         //create reverse map for lookup 
         val genre2Ind = (genreKeys zip (0 until genreKeys.length)) toMap
         
+        Logger.info("created genres to index map, numGenres: " + numGenres 
+                    + " numItems: " + itemSet.size)
         
-        //get RDDs of items itemGenreList: itemId, subgenre, Genre        
+        //save genre mapping to indexes
+        var featureMapFileName = jobInfo.resourceLoc(RecJob.ResourceLoc_JobFeature) + "/"+ resourceIden + "Map"
+        Logger.logger.info("Dumping featureMap resource: " + featureMapFileName)
+        genreInd2KeyDescRDD.saveAsTextFile(featureMapFileName)
+        
+        //get RDDs of items itemGenreList: itemId, subgenre, Genre   
+        //filter only those for which genre is already found
         fileName = jobInfo.resourceLoc(RecJob.ResourceLoc_RoviHQ) + jobInfo.trainDates(0) + "/program_genre*"
         var itemGenreList = sc.textFile(fileName).map { line =>
               val fields = line.split('|')
-              (fields(ITEM_ID_IND), (genre2Ind(fields(ITEM_SUB_GENRE_IND)), 
-                  genre2Ind(fields(ITEM_GENRE_IND))) )             
-            }.filter(x => itemSet(x._1) )
+              (fields(ItemIdInd),  fields(ItemGenreInd))             
+            }.filter(x => itemSet(x._1) && genre2Ind.contains(x._2)) 
+         
+        Logger.info("Creating item genrelist")
         
         //parse program genre file for each date and find genres for items in train
         for (trainDate <- jobInfo.trainDates.tail) {
             fileName = jobInfo.resourceLoc(RecJob.ResourceLoc_RoviHQ) + trainDate + "/program_genre*"
             val nextGenre = sc.textFile(fileName).map { line =>
               val fields = line.split('|')
-              (fields(ITEM_ID_IND), (genre2Ind(fields(ITEM_SUB_GENRE_IND)), 
-                  genre2Ind(fields(ITEM_GENRE_IND))) )             
-            }.filter(x => itemSet(x._1) )
+              (fields(ItemIdInd), fields(ItemGenreInd))              
+            }.filter(x => itemSet(x._1) && genre2Ind.contains(x._2))
             itemGenreList = itemGenreList.union(nextGenre).distinct
         }  
-        
+       
+        Logger.info("created itemGenres list")
+            
         //generate feature vector for each items    
         val itemGenreInds = itemGenreList.groupByKey().map { x =>
           var itemId = x._1
           var featureVec = Array.fill[Byte](numGenres)(0) //initialize all to 0
-          for ((subGenreInd, genreInd) <- x._2) {
-              featureVec(subGenreInd) = 1
-              featureVec(genreInd) = 1
+          for (genre <- x._2) {
+              featureVec(genre2Ind(genre)) = 1
           }
+          (itemId, featureVec.mkString(","))
         }
         
+        Logger.info("created item genre feature vectors")
+        
         //save item features as textfile
-        var featureFileName = jobInfo.resourceLoc(RecJob.ResourceLoc_JobFeature) + "/"+ resourceIden
-        var featureMapFileName = jobInfo.resourceLoc(RecJob.ResourceLoc_JobFeature) + "/"+ resourceIden + "Map"
+        var featureFileName = jobInfo.resourceLoc(RecJob.ResourceLoc_JobFeature) + "/" + resourceIden
         Logger.logger.info("Dumping feature resource: " + featureFileName)
         itemGenreInds.saveAsTextFile(featureFileName)
-        Logger.logger.info("Dumping featureMap resource: " + featureMapFileName)
-        genreInd2KeyDescRDD.saveAsTextFile(featureMapFileName)
      
         // 4. Generate and return a FeatureResource that includes all resources.  
         val resourceMap:HashMap[String, Any] = new HashMap()
-        resourceMap(FeatureResource.ResourceStr_UserFeature) = featureFileName
-        resourceMap(FeatureResource.ResourceStr_UserFeatureMap) = featureMapFileName
+        resourceMap(FeatureResource.ResourceStr_ItemFeature) = featureFileName
+        resourceMap(FeatureResource.ResourceStr_ItemFeatureMap) = featureMapFileName
+        
+        Logger.info("Saved item features and feature map")
         
         new FeatureResource(true, resourceMap, resourceIden)
     }
