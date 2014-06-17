8cf12e8231a89ee7d3efdb7e11b8fd9c6ffe9a22
Mohit Sharma <sharm163@umn.edu>
2014-06-17 14:03:35 -0700

    started adding userFeature behavior

--- src/main/scala/com/samsung/vddil/recsys/feature/user/UserFeatureBehaviorGenre.scala
+++ src/main/scala/com/samsung/vddil/recsys/feature/user/UserFeatureBehaviorGenre.scala
@@ -11,14 +11,26 @@ object UserFeatureBehaviorGenre extends FeatureProcessingUnit{
 	def processFeature(featureParams:HashMap[String, String], jobInfo:RecJob):FeatureResource = {
 		Logger.logger.error("%s has not been implmented.".format(getClass.getName()))
 		
+		//get spark context
+		val sc = jobInfo.sc
+		
 		// 1. Complete default parameters
 		
 		
 	    // 2. Generate resource identity using resouceIdentity()
-		
+		var resourceIden = resourceIdentity(featureParams)
 		
 	    // 3. Feature generation algorithms (HDFS operations)
 		
+		//get item genres
+		
+		val mergedData = sc.textFile(jobInfo.jobStatus.resourceLocation_CombineData)
+						   .map { line =>
+						   			val fields = line.split(',')
+						   			//user,item, watchtime
+						   			val item = fields(2)
+						   			(fields(0), fields(1), fields(2)) 
+						 }
 		
 	    // 4. Generate and return a FeatureResource that includes all resources.  
 		FeatureResource.fail
