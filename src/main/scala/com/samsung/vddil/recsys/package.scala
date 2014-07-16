d9b9db4baf2202f3daf5a0195579d6b11e9472f8
Jiayu Zhou <dearjiayu@gmail.com>
2014-07-15 18:15:32 -0700

    Remove config file and add Pipeline documentation

---
+++ src/main/scala/com/samsung/vddil/recsys/package.scala
@@ -0,0 +1,11 @@
+package com.samsung.vddil
+
+/**
+ * The Samsung recommender system.
+ * 
+ * ==Overview==
+ * The main entrance of the program is [[com.samsung.vddil.recsys.Pipeline]]. 
+ * 
+ * Use [[com.samsung.vddil.recsys.Logger]] to log information. 
+ */
+package object recsys 
