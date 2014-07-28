d2981df7b8d1f959ec1eaf0c762b4d234226b407
Jiayu Zhou <dearjiayu@gmail.com>
2014-07-28 13:12:25 -0700

    Code consolidation: Model

--- src/main/scala/com/samsung/vddil/recsys/model/ModelHandler.scala
+++ src/main/scala/com/samsung/vddil/recsys/model/ModelHandler.scala
@@ -2,6 +2,9 @@ package com.samsung.vddil.recsys.model
 
 import com.samsung.vddil.recsys.job.RecJob
 import scala.collection.mutable.HashMap
+import com.samsung.vddil.recsys.utils.Logger
+import com.samsung.vddil.recsys.model.regression.RegressionModelRidge
+import com.samsung.vddil.recsys.model.regression.RegressionModelFactorizationMachine
 
 /**
  * The model handler implementations are used to process a specified type of model.  
@@ -15,4 +18,64 @@ trait ModelHandler {
 	 * information to jobInfo.jobStatus 
 	 */
 	def buildModel(modelName:String, modelParam:HashMap[String, String], dataResourceStr:String, jobInfo:RecJob): Boolean
+}
+
+
+object RegressionModelHandler extends ModelHandler {
+	//predefined values for model name
+	val RegModelRidge:String = "ridge_reg"
+	val RegModelLasso:String = "lasso_reg"
+	val RegModelFML2:String  = "fm_l2_reg"
+	
+	def buildModel(modelName:String, modelParams:HashMap[String, String], dataResourceStr:String, jobInfo:RecJob): Boolean = {
+	    
+		Logger.info("Processing regression model [%s:%s]".format(modelName, modelParams))
+	  
+	    var resource:ModelResource = ModelResource.fail
+		modelName match{
+		  case RegModelRidge => resource = RegressionModelRidge.learnModel(modelParams, dataResourceStr, jobInfo)
+		  //case RegModelLasso => resource = ModelResource.fail
+		  case RegModelFML2  => resource = RegressionModelFactorizationMachine.learnModel(modelParams, dataResourceStr, jobInfo)
+		  case _ => Logger.warn("Unknown regression model name [%s]".format(modelName))
+		}
+	  
+		//For the successful ones, push resource information to jobInfo.jobStatus
+		if(resource.success){
+		   resource.resourceMap(ModelResource.ResourceStr_RegressModel) match{
+		     case model:ModelStruct => 
+		       jobInfo.jobStatus.resourceLocation_RegressModel(resource.resourceIden) = model
+		   }
+		}
+	    
+		// return if the resource is successful or not.
+	    resource.success
+	}
+}
+
+object ClassificationModelHandler extends ModelHandler {
+	val ClsModelLogisticL2:String = "lrl2_cls"
+	val ClsModelLogisticL1:String = "lrl1_cls"
+  
+	def buildModel(modelName:String, modelParams:HashMap[String, String], dataResourceStr:String, jobInfo:RecJob): Boolean = {
+	    
+		Logger.info("Processing classification model [%s:%s]".format(modelName, modelParams))
+	  
+	    var resource:ModelResource = ModelResource.fail
+		modelName match{
+		  //case ClsModelLogisticL2 => resource = ModelResource.fail
+		  //case ClsModelLogisticL1 => resource = ModelResource.fail
+		  case _ => Logger.warn("Unknown classification model name [%s]".format(modelName))
+		}
+	  
+		//For the successful ones, push resource information to jobInfo.jobStatus
+		if(resource.success){
+		   resource.resourceMap(ModelResource.ResourceStr_ClassifyModel) match{
+		     case model:ModelStruct => 
+		       jobInfo.jobStatus.resourceLocation_ClassifyModel(resource.resourceIden) = model
+		   }
+		}
+	    
+		// return if the resource is successful or not.
+	    resource.success
+	}
 }
--- src/main/scala/com/samsung/vddil/recsys/model/ModelProcessingUnit.scala
+++ src/main/scala/com/samsung/vddil/recsys/model/ModelProcessingUnit.scala
@@ -53,3 +53,32 @@ trait ModelProcessingUnit {
 	//def saveModel(modelFileName: String, sc: SparkContext)
 	//def getModel(modelFileName: String, sc: SparkContext)
 }
+
+/**
+ * This is the results returned by a model processing unit. 
+ * 
+ * success:Boolean stores if this feature is successful
+ * 
+ * resourceMap: stores a (key, resource) pair to be handled by ModelHandler. 
+ * 
+ * resourceIden: a unique identifier for this model (to be used in HashMap purpose).
+ * 
+ * 
+ */
+class ModelResource(var success:Boolean, var resourceMap:HashMap[String, Any] = new HashMap, var resourceIden:String) {
+    
+}
+
+object ModelResource{
+   val ResourceStr_RegressModel  = "regModel"
+   val ResourceStr_RegressPerf   = "regPerf"
+   val ResourceStr_ClassifyModel = "clsModel"
+   val ResourceStr_ClassifyPerf  = "clsPerf"
+    
+   /**
+    * Provides a shortcut with an empty ModelResource indicating a failed status.
+    */
+   def fail:ModelResource = {
+      new ModelResource(false, null, "")
+   }
+}
