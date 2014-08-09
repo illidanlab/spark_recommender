5aea75fa7e1a198faf866f37be809fd1f0a736a9
zhouyin <yin.zhou@samsung.com>
2014-08-08 18:29:26 -0700

    Geo feature at largescale on yarn

--- src/main/scala/com/samsung/vddil/recsys/feature/user/UserFeatureDemographicGeoLocationNew.scala
+++ src/main/scala/com/samsung/vddil/recsys/feature/user/UserFeatureDemographicGeoLocationNew.scala
@@ -11,11 +11,12 @@ import com.samsung.vddil.recsys.linalg.Vectors
 import org.apache.spark.SparkContext._
 import scala.util.control._
 import com.samsung.vddil.recsys.feature.UserFeatureStruct
-
 import scala.util.Marshal
 import scala.io.Source
 import scala.collection.immutable
 import java.io._
+import scala.collection.Parallel
+import org.apache.tools.ant.taskdefs.Parallel
 
 /*
  * User Feature: Geo Location and Demographic features. 
@@ -59,8 +60,10 @@ object UserFeatureDemographicGeoLocationNew extends FeatureProcessingUnit {
 		    
 		Logger.info("Geo data location: " + geoLoc)
 
-		val demographicsinfo = sc.textFile(demogrphicsLoc + "/" + "demographics_zipcode_new2.txt")		
+		val demographicsinfo = sc.textFile(demogrphicsLoc + "/" + "demographics_zipcode_new_full.txt") // change to full version		
 		Logger.info("We have found " + demographicsinfo.count() + " lines in demographics.")			
+		
+		var accum = sc.accumulator(0)
 		
 		var demographicHashTable = demographicsinfo.filter{
 		    line => hasAllFields(line.split("\t"),line.split("\t").size == 55)
@@ -69,16 +72,21 @@ object UserFeatureDemographicGeoLocationNew extends FeatureProcessingUnit {
             val fields      = line.split("\t")
             var zipcode     = fields(0)
             var demoFeature = fields.tail.map{_.toDouble}
+            accum += 1
             (zipcode,demoFeature)
         }.collectAsMap
+        println("The demographic HashTable contains " + accum + " records.")
+        val demographicHashTableRDD = sc.broadcast(demographicHashTable)
         
+        /*
 		Logger.info("Lets start to build geo features on " + trainDate.mkString(","))
 		val geoDataOneDay = sc.textFile(geoLoc + "/" + trainDate(0))
 		Logger.info("We have found " + geoDataOneDay.count() + " lines in day " + trainDate(0) + " .")
+		* */
 		
 		val userFeatureMap = trainDate.map{
             date => 
-			val geoDataOneDay = sc.textFile(geoLoc + "/" + date)		
+			val geoDataOneDay = sc.textFile(geoLoc + "/" + date + "/duid_geo.tsv")		
 			Logger.info("We have found " + geoDataOneDay.count() + " lines in day " + date + " .")	
 			
 			var userFeatureMapOneDay = geoDataOneDay.filter{
@@ -86,18 +94,18 @@ object UserFeatureDemographicGeoLocationNew extends FeatureProcessingUnit {
 			    val fields  = line.split("\t")
 			    var duid    = fields(0)
 			    var zipcode = fields(5) // the sixth fields in the duid_geo.tsv
-			    demographicHashTable.isDefinedAt(zipcode)
+			    demographicHashTableRDD.value.isDefinedAt(zipcode)
 			}.map{
 			    line        =>
 			    val fields  = line.split("\t")
 			    var duid    = fields(0)
 			    var zipcode = fields(5)		    
-			    (duid,demographicHashTable(zipcode))
+			    (duid,demographicHashTableRDD.value(zipcode))
 			}
 			userFeatureMapOneDay
         }.reduce{ (a,b) =>
             a.union(b)
-        }.distinct.collect
+        }.distinct
         
         for (tt <- userFeatureMap) {
             println(tt._1 + " " + tt._2.toSeq.toString)
@@ -107,24 +115,41 @@ object UserFeatureDemographicGeoLocationNew extends FeatureProcessingUnit {
         println("resourceIden : " + resourceIden)
         println("featureMapFileName : " + featureMapFileName)
         
-        
-	    // 4. Generate and return a FeatureResource that includes all resources.  
+       
         //save user geo demographic features as textfile
-        /*
+        val demographicsDescription = sc.textFile(demogrphicsLoc + "/" + "demographics_description.txt")
+        val lenDescription = demographicsDescription.count.toInt
+        val demographicsDescriptionMap = ((0 until lenDescription) zip demographicsDescription.collect).toMap
+        val demographicsDescriptionMapRDD = sc.makeRDD(demographicsDescriptionMap.toList)
+        for (tt <- (0 until lenDescription))
+        	println(tt + " -> " + demographicsDescriptionMap(tt))
+        
+        
+    	//save item features as textfile
         if (jobInfo.outputResource(featureFileName)){
         	Logger.logger.info("Dumping feature resource: " + featureFileName)
         	userFeatureMap.saveAsObjectFile(featureFileName) //directly use object + serialization. 
         }		
-		
-		Logger.info("saved! featureFileName is " + featureFileName)
-
+        
+        //save genre mapping to indexes
+        if (jobInfo.outputResource(featureMapFileName)){
+        	Logger.logger.info("Dumping featureMap resource: " + featureMapFileName)
+        	demographicsDescriptionMapRDD.map{
+        	    x => x._1 + "," + x._2
+        	}.saveAsTextFile(featureMapFileName)
+        }
+        
+        
+    	// 4. Generate and return a FeatureResource that includes all resources.  
 		val featureStruct:UserFeatureStruct = 
   			new UserFeatureStruct(IdenPrefix, resourceIden, featureFileName, featureMapFileName)
+        	
+        val resourceMap:HashMap[String, Any] = new HashMap()
+        resourceMap(FeatureResource.ResourceStr_UserFeature) = featureStruct
+		Logger.info("Saved item features and feature map")	
 		
 		new FeatureResource(true, Some(resourceMap), resourceIden)
-		* 
-		*/
-		FeatureResource.fail
+	
     }
     val IdenPrefix:String = "UserFeatureGeo"
 }
