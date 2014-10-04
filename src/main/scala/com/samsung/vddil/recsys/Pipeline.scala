f5710615eae655f2eb8dd33c7e410aa31838084f
Jiayu Zhou <dearjiayu@gmail.com>
2014-10-04 01:37:02 -0700

    Revert spark configuration

--- src/main/scala/com/samsung/vddil/recsys/Pipeline.scala
+++ src/main/scala/com/samsung/vddil/recsys/Pipeline.scala
@@ -135,10 +135,12 @@ object Pipeline {
 	    	 conf.set("spark.executor.extraJavaOptions ", "-XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError")
 	    	 conf.set("spark.serializer",                 "org.apache.spark.serializer.KryoSerializer")
 	    	 conf.set("spark.kryo.registrator",           "com.samsung.vddil.recsys.SerializationRegistrator")
-	    	 conf.set("spark.kryoserializer.buffer.mb",   "500") //enable this if get Kryo Buffer Overflow
-	    	 conf.set("spark.akka.frameSize",             "1024")
-             conf.set("spark.akka.timeout",               "1000")
-             conf.set("spark.core.connection.ack.wait.timeout", "600")
+	    	 //conf.set("spark.kryoserializer.buffer.mb",   "500") //enable this if get Kryo Buffer Overflow
+	    	 conf.set("spark.akka.frameSize",             "100")
+             conf.set("spark.akka.timeout",               "200")
+//	    	 conf.set("spark.akka.frameSize",             "1024")
+//             conf.set("spark.akka.timeout",               "1000")
+//             conf.set("spark.core.connection.ack.wait.timeout", "600")
              //conf.set("spark.speculation",                "true") //this is done outside. 
 	    	 
 	         try{
