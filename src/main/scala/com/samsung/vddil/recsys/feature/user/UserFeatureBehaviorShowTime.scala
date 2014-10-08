cf5fde927b81d4137cc05c271cb28a70c0fefefd
Jiayu Zhou <dearjiayu@gmail.com>
2014-10-08 01:08:27 -0700

    Adding showTime and channel features

---
+++ src/main/scala/com/samsung/vddil/recsys/feature/user/UserFeatureBehaviorShowTime.scala
@@ -0,0 +1,34 @@
+package com.samsung.vddil.recsys.feature.user
+
+import org.apache.spark.SparkContext._
+import scala.collection.mutable.HashMap
+import com.samsung.vddil.recsys.feature.FeatureProcessingUnit
+import com.samsung.vddil.recsys.feature.FeatureResource
+import com.samsung.vddil.recsys.feature.item.ItemFeatureShowTime
+import com.samsung.vddil.recsys.feature.UserFeatureStruct
+import com.samsung.vddil.recsys.job.Rating
+import com.samsung.vddil.recsys.job.RecJob
+import com.samsung.vddil.recsys.linalg.{Vector,Vectors,SparseVector}
+import com.samsung.vddil.recsys.Pipeline
+import com.samsung.vddil.recsys.utils.HashString
+import com.samsung.vddil.recsys.utils.Logger
+
+
+object UserFeatureBehaviorShowTime extends FeatureProcessingUnit 
+                                with UserFeatureItemWtAgg {
+	
+  def processFeature(featureParams:HashMap[String, String], jobInfo:RecJob):FeatureResource = {
+		
+    //Generate resource identity using resouceIdentity()
+		val dataHashingStr = HashString.generateOrderedArrayHash(jobInfo.trainDates)
+		var resourceIden = resourceIdentity(featureParams,dataHashingStr)
+		var featureFileName    = jobInfo.resourceLoc(RecJob.ResourceLoc_JobFeature) + 
+								    "/" + resourceIden
+
+    generateFeature(featureParams, jobInfo, ItemFeatureShowTime.checkIdentity,
+      featureFileName, IdenPrefix, resourceIden)
+
+	}
+	
+	val IdenPrefix:String = "UserFeatureShowTime"
+}
