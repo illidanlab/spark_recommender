0f2515068afaed83b6b824e0886ac5a30ff53a8a
Jiayu Zhou <dearjiayu@gmail.com>
2014-10-14 17:51:30 -0700

    FIX: EMR job submit is now fully functional

--- submit_emr.sh
+++ submit_emr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ $# -lt 3 ]; then
-	echo "Usage: submit_emr.sh <job_file> <num_cores> <num_executors> [compile]"
+	echo "Usage: ./submit_emr.sh <job_file> <node_num> <num_executors> [compile]"
+	echo "Example: ./submit_emr.sh ~/Desktop/emr/emr_jobs/aws_test_job_s3_st.xml 50 20 [compile]"
 	exit -1
 fi
 
@@ -11,18 +12,23 @@ set -e
 set -x
 
 cluster_name="recommender-system-pipeline"
+cluster_node_num=$2
+
 job_file=$1
-memory_driver="7G"
-memory_exeutor="7G"
-num_cores=$2
+memory_driver="10G"
+memory_exeutor="10G"
+num_cores="6"
 num_executors=$3
 
+spark_default_parallelism=1600
+running_job_file="running_job.xml"
+
 #set up directory
-emr_dir=$HOME/emr/
+emr_dir=$HOME/emr
 
 #compile
 if [ $# -ge 4 ]; then
-    sbt clean assembly
+    sbt clean package
 fi
 
 #upload program
@@ -35,11 +41,10 @@ else
 	exit -2
 fi
 
-
 #upload jobfile
 echo "uploading job file to S3"
 if [ -f $job_file ]; then 
-	s3cmd put $job_file s3://vddil.recsys/testScript/aws_test_job.xml
+	s3cmd put $job_file s3://vddil.recsys/testScript/$running_job_file
 else
 	echo "Cannot find file: $job_file, abort"
 	exit -3
@@ -47,19 +52,37 @@ fi
 
 ##start cluster
 cluster_result=$($emr_dir/elastic-mapreduce --create --name $cluster_name --ami-version 3.1.1 \
-        --instance-group master --instance-count 1 --instance-type c3.2xlarge \
-        --instance-group core --instance-count 50 --instance-type c3.4xlarge \
-        --bootstrap-action s3://fbaldo-analytics/steps/spark/1.0.2/spark_1.0.2.py \
-        	--args "BA" \
+        --instance-group master --instance-count 1 --instance-type c3.4xlarge \
+        --instance-group core --instance-count $cluster_node_num --instance-type c3.4xlarge \
+		--bootstrap-action s3://elasticmapreduce/bootstrap-actions/install-ganglia \
         --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop \
         	--args "-y,yarn.log-aggregation-enable=true,-y,yarn.log-aggregation.retain-seconds=-1,-y,yarn.log-aggregation.retain-check-interval-seconds=3000,-y,yarn.nodemanager.remote-app-log-dir=/tmp/logs" \
-        --jar s3://elasticmapreduce/libs/script-runner/script-runner.jar --args "s3://fbaldo-analytics/steps/spark/1.0.2/spark_1.0.2.py,STEP" \
-        --pig-interactive --alive --region us-east-1 --key-pair DMC_DEV)
+        --bootstrap-action s3://support.elasticmapreduce/spark/install-spark \
+        --jar s3://elasticmapreduce/libs/script-runner/script-runner.jar \
+            --args "s3://support.elasticmapreduce/spark/start-history-server" \
+        --jar s3://elasticmapreduce/libs/script-runner/script-runner.jar \
+            --args "s3://support.elasticmapreduce/spark/configure-spark.bash,spark.default.parallelism=$spark_default_parallelism,spark.locality.wait.rack=0" \
+        --alive --region us-east-1 --key-pair DMC_DEV)
 
 #get cluster master DNS
 cluster_id=$(echo $cluster_result | cut -d" " -f4)
 echo "Cluster ID: $cluster_id"
 
+echo "*To SSH to the master:"
+echo "  $emr_dir/elastic-mapreduce -j $cluster_id --ssh"
+echo " "
+echo "*SSH Tunnel for Web Interface"
+echo "  $emr_dir/elastic-mapreduce -j $cluster_id --sock"
+echo " "
+echo "*Hadoop Web Interface (check )"
+echo "  http://master_dns:9026/cluster/app/<application_id>"
+echo "*Spark Web Interface for Stages"
+echo "  http://master_dns:9046/proxy/<application_id>/"
+echo "*Spark Web Interface for Stages"
+echo "  http://master_dns:18080/"
+echo " "
+echo "*Remeber to TERMINATE the alive cluster when it is done."
+echo "  $emr_dir/elastic-mapreduce --terminate $cluster_id"
 
 ##submit jobs 
 
@@ -71,33 +94,41 @@ echo "Cluster ID: $cluster_id"
 #         --args "--src,s3://vd.infra.dev.dms.spark/dms_raw_data/duid-program-watchTime,--dest,/duid-program-watchTime"
 
 # STEP: run the main program 
-$emr_dir/elastic-mapreduce -j $cluster_id --region us-east-1 --jar s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar \
-         --args "s3://vddil.recsys/testScript/aws_test_t3.sh,$memory_driver,$memory_exeutor,$num_cores,$num_executors"
+#$emr_dir/elastic-mapreduce -j $cluster_id --region us-east-1 --jar s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar \
+        #         --args "s3://vddil.recsys/testScript/aws_test_t3.sh,$memory_driver,$memory_exeutor,$num_cores,$num_executors"
+
+$emr_dir/elastic-mapreduce -j $cluster_id --region us-east-1 \
+	--jar s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar \
+	--args "s3://vddil.recsys/testScript/aws_run_job.sh,$running_job_file,$memory_driver,$memory_exeutor,$num_cores,$num_executors"
 
 # STEP: copy results back. 
-$emr_dir/elastic-mapreduce -j $cluster_id --region us-east-1 --jar /home/hadoop/lib/emr-s3distcp-1.0.jar \
-         --args "--src,/workspace,--dest,s3://vddil.recsys/workspace"
+#$emr_dir/elastic-mapreduce -j $cluster_id --region us-east-1 --jar /home/hadoop/lib/emr-s3distcp-1.0.jar \
+        #         --args "--src,/workspace,--dest,s3://vddil.recsys/workspace"
 
-#display information
-read -p "Wait until master DNS shown in web, and press [Enter] key to continue..."
+echo "Logging to the master. Please get application id using *yarn application -list* and disconnect to continue. "
+$emr_dir/elastic-mapreduce -j $cluster_id --ssh
 
+#display information 
+# BUG: these information are gone somehow. 
 master_dns=$($emr_dir/elastic-mapreduce --describe $cluster_id | grep MasterPublicDnsName | cut -d"\"" -f4)
 echo "Master DNS: $master_dns"
 
 echo "*To SSH to the master:"
-echo "  aws emr ssh --cluster-id $cluster_id --key-pair-file ~/DMC_DEV.pem"
-echo "  OR) ssh hadoop@$master_dns -i ~/DMC_DEV.pem"
+echo "  $emr_dir/elastic-mapreduce -j $cluster_id --ssh"
 echo " "
 echo "*SSH Tunnel for Web Interface"
-echo "  aws emr socks --cluster-id $cluster_id --key-pair-file ~/DMC_DEV.pem"
-echo "  OR) ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -ND 8157 -i ~/DMC_DEV.pem hadoop@$master_dns"
+echo "  $emr_dir/elastic-mapreduce -j $cluster_id --sock"
 echo " "
 echo "*Hadoop Web Interface (check )"
 echo "  http://$master_dns:9026/cluster/app/<application_id>"
 echo "*Spark Web Interface for Stages"
 echo "  http://$master_dns:9046/proxy/<application_id>/"
+echo "*Spark Web Interface for Stages"
+echo "  http://$master_dns:18080/"
 echo " "
 echo "*Remeber to TERMINATE the alive cluster when it is done."
 echo "  $emr_dir/elastic-mapreduce --terminate $cluster_id"
+
+#$emr_dir/elastic-mapreduce -j $cluster_id --sock
 
 exit 0
